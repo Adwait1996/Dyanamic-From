50df216448922e603d6edf75e9f12ebba1edc50a
Adwait1996 <30655428+Adwait1996@users.noreply.github.com>
2017-11-04 15:28:47 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+	function input_fields($type,$label_array,$width)
+	{
+		
+
+			foreach ($label_array as $label=> $addon)
+			{
+			echo "
+			<div class='form-group'>
+				<div class='col-$width'>
+					<label for='$label'>".ucfirst($label)."</label>
+					<div class='input-group'>
+	                      <div class='input-group-addon'><i class='fa fa-$addon fa-fw' aria-hidden='true'></i></div>
+						<input type='$type' class='form-control' name='$label' id='$label'>
+					</div>
+				</div>
+			</div>
+			";
+	}
+	}
+	function get_theme()
+	{
+		$theme_array = array('yeti','united','solar','darkly','litera','simplex','materia','cosmo','cyborg','lumen','pulse','sketchy','spacelab','superhero','slate','minty','cerulean','flatly','lux','journal');
+		$index = rand(0,sizeof($theme_array));
+		return $theme_array[$index];
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>test</title>
+	<link rel="stylesheet" href="https://bootswatch.com/4/<?=get_theme();?>/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+	<?php include_once "header.php";?>
+	<form action="submit.php" method="post">
+	<?php 
+	   $labels = array('name'=>'user','email'=>'envelope','phone'=>'phone','password'=>'lock','username'=>'user-plus');
+		input_fields('text',$labels,6);
+			?>
+			<div class='form-group'>
+				<div class='col-2'>
+			<button class="btn btn-success"> submit</button>
+		</div>
+	</div>
+		</form>
+</body>
+</html>
